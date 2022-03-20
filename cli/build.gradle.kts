6f59e12c3fe38037500e73e436cd1d63ae7df87b
Mezentsev Denis <mezentsev.d@spb-ifuture.com>
2022-03-20 13:18:45 +0300

    21-add-picocli-completion-to-distribution

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -17,3 +17,25 @@ application {
     mainClass.set("mezlogo.template_cli.cli.MainCommand")
     applicationName = "mycliapp"
 }
+
+val generateAutocomplete by tasks.registering(JavaExec::class) {
+    val completionsDir = layout.buildDirectory.dir("completions")
+    outputs.dir(completionsDir)
+    args = listOf("-f", "-o", completionsDir.get().file("mycliapp_completion").asFile.absolutePath, "mezlogo.template_cli.cli.MainCommand")
+    mainClass.set("picocli.AutoComplete")
+    classpath = sourceSets.main.get().runtimeClasspath
+}
+
+val startScripts by tasks.named("startScripts") {
+    dependsOn(generateAutocomplete)
+}
+
+distributions {
+    main {
+        contents {
+            from(generateAutocomplete) {
+                into("completions")
+            }
+        }
+    }
+}
